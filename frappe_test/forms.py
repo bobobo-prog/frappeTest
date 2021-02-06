7f15bf37e6dacac11d1d7d5533f360bf2550581b
bobobo-prog <ashwin.mp@somaiya.edu>
2021-02-06 16:19:05 +0530

    Forms Added

--- frappe_test/forms.py
+++ frappe_test/forms.py
@@ -1,6 +1,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
+from wtforms.fields.core import IntegerField, TimeField
 from wtforms.validators import DataRequired, data_required,Length
 
 class AddProductForm(FlaskForm):
@@ -12,3 +13,11 @@ class AddLocationForm(FlaskForm):
     loc_id = StringField('Location ID',validators=[DataRequired(),Length(max = 4,min = 4)])
     loc_name = StringField('Address',validators=[DataRequired(),Length(max = 20)])
     submit = SubmitField('Add Location') 
+
+class AddProdMovement(FlaskForm):
+    movement_id = StringField('Movement ID',validators=[DataRequired(),Length(max = 4,min = 4)])
+    from_location = StringField('From Location',validators=[DataRequired()])
+    to_location = StringField('To Location',validators=[DataRequired()])
+    product_id = StringField('Product ID',validators=[DataRequired(),Length(max = 4,min = 4)])
+    quantity = IntegerField('Quantity',validators=[DataRequired()])
+    submit = SubmitField('Add Movement')
--- frappe_test/routes.py
+++ frappe_test/routes.py
@@ -1,8 +1,9 @@
 from flask import Flask,render_template,flash,redirect
 from flask.helpers import url_for
-from frappe_test.models import Product,ProductMovement
+from frappe_test.models import Location, Product,ProductMovement
 from frappe_test import app
-from frappe_test.forms import AddProductForm,AddLocationForm
+from frappe_test.forms import AddProdMovement, AddProductForm,AddLocationForm
+from frappe_test import db
 
 @app.route("/")
 def home():
@@ -12,6 +13,9 @@ def home():
 def add_product():
     form = AddProductForm()
     if form.validate_on_submit():
+        product = Product(product_id = form.prod_id.data,product_name = form.prod_name.data)
+        db.session.add(product)
+        db.session.commit()
         flash(f'{form.prod_name.data} Added to Inventory!','success')
         return redirect(url_for('add_product'))
 
@@ -21,7 +25,24 @@ def add_product():
 def add_location():
     form = AddLocationForm()
     if form.validate_on_submit():
-        flash(f'{form.loc_id.data} Added to Inventory!','success')
+        location = Location(location_id = form.loc_id.data,address = form.loc_name.data)
+        db.session.add(location)
+        db.session.commit()
+        flash(f'{form.loc_id.data} Added to Database!','success')
         return redirect(url_for('add_location'))
 
     return render_template('addlocation.html',title = 'Add Location',form = form)
+
+
+@app.route("/addproductmovement",methods = ['GET','POST'])
+def add_prodmovement():
+    form = AddProdMovement()
+    if form.validate_on_submit():
+        movement = ProductMovement(movement_id = form.movement_id.data,from_location = form.from_location.data,to_location = form.to_location.data,product_id = form.product_id.data,quantity = form.quantity.data)
+        db.session.add(movement)
+        db.session.commit()
+        flash(f'{form.movement_id.data} Added to Database!','success')
+        return redirect(url_for('add_prodmovement'))
+
+    return render_template('addproductmovement.html',title = 'Add Movement Data',form = form)
+    
